1c4025363e6b84097841821a4e49155fe15fd5c7
jovan7297 <82431965+jovan7297@users.noreply.github.com>
2023-02-09 14:10:41 -0500

    Add files via upload

---
+++ LabProblem2.java
@@ -0,0 +1,52 @@
+//Jovanni Lozzi
+//Problem 2
+
+package LabProblem2;
+
+import javax.swing.JOptionPane;
+
+
+public class LabProblem2 
+{//Class Begins
+
+    public static void main(String[] args) 
+    {//Main Method Begins
+     
+        
+            
+    String item1 = JOptionPane.showInputDialog("Input the name of item 1");
+    String s1 = JOptionPane.showInputDialog("What is the price of item 1");    
+    Double price1 = Double.parseDouble(s1);
+      
+    String item2 = JOptionPane.showInputDialog("Input the name of item 2");
+    s1 = JOptionPane.showInputDialog("What is the price of item 2");   
+    Double price2 = Double.parseDouble(s1);
+    
+    String item3 = JOptionPane.showInputDialog("Input the name of item 3");
+    s1 = JOptionPane.showInputDialog("What is the price of item 3");   
+    Double price3 = Double.parseDouble(s1);
+    
+    System.out.println(item1+":"+price1);
+    System.out.println(item2+":"+price2);
+    System.out.println(item3+":"+price3);
+    
+    double average = (price1 +price2 + price3)/3;
+    
+    String peas = ("peas");
+    
+    int x =0;
+    
+    if (item1.equals(peas))
+        x ++;
+    if (item2.equals(peas))
+        x ++;
+    if (item3.equals(peas))
+        x ++;
+    
+    if (x > 0)
+        System.out.println("The average cost of the price is "+ average);
+    else
+        System.out.println("no average output");
+    
+    }//Main Method Ends  
+}//Class Ends
